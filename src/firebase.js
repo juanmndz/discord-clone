5cf263f12168dc4ee6b426e31efcfcc12fe645f7
Juan Mendez <juanmndz19@gmail.com>
2021-01-24 17:19:17 -0700

    Firebase security

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,29 @@
-import firebase from 'firebase'
-
+import firebase from "firebase";
+const {
+  REACT_APP_apiKey,
+  REACT_APP_authDomain,
+  REACT_APP_databaseURL,
+  REACT_APP_projectId,
+  REACT_APP_storageBucket,
+  REACT_APP_messagingSenderId,
+  REACT_APP_appId,
+  REACT_APP_measurementId,
+} = process.env;
 const firebaseConfig = {
-    apiKey: "AIzaSyBLsayG6LM8uRVxks2gP1Q81pKlpnIct_I",
-    authDomain: "discordapp-b6daf.firebaseapp.com",
-    databaseURL: "https://discordapp-b6daf.firebaseio.com",
-    projectId: "discordapp-b6daf",
-    storageBucket: "discordapp-b6daf.appspot.com",
-    messagingSenderId: "459221834734",
-    appId: "1:459221834734:web:b7709a7e66a1b0a6418cff",
-    measurementId: "G-N6SP0DTPGS"
-  };
-  
-  const firebaseApp = firebase.initializeApp(firebaseConfig)
-  const db = firebaseApp.firestore()
-  const auth = firebase.auth()
-  const provider = new firebase.auth.GoogleAuthProvider()
+  apiKey: REACT_APP_apiKey,
+  authDomain: REACT_APP_authDomain,
+  databaseURL: REACT_APP_databaseURL,
+  projectId: REACT_APP_projectId,
+  storageBucket: REACT_APP_storageBucket,
+  messagingSenderId: REACT_APP_messagingSenderId,
+  appId: REACT_APP_appId,
+  measurementId: REACT_APP_measurementId,
+};
 
-  export {auth, provider}
-  export default db
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
